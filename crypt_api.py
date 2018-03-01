7b291fd2ff2c4393a114752ef034ddd8a98237a9
KC <weilihmen@gmail.com>
2018-03-01 18:16:07 +0800

    draft

--- crypt_api.py
+++ crypt_api.py
@@ -5,11 +5,11 @@ index01=200
 index02=['BTC','RLC','TEL','PPP','WRONGTEST']
 
 def rank_200():
-	req = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/')
-	req2 = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/?start=100&limit=100')
+	req = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=TWD')
+	req2 = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=TWD&start=100&limit=100')
 	new_data = json.loads(req.text)
-	for a_coin in json.loads(req2.text):
-		new_data.append(a_coin)
+	for coin in json.loads(req2.text):
+		new_data.append(coin)
 	return new_data
 
 def group_rising_and_falling(data,num,group_size):
@@ -17,17 +17,61 @@ def group_rising_and_falling(data,num,group_size):
 	newlist = sorted(group_data, key=lambda x: (float(x["percent_change_1h"]) >= 0, (float(x["percent_change_1h"])))) 
 	return newlist[-(num):],newlist[:num]
 
-data=rank_200()
-top,last=group_rising_and_falling(data, 3, 100)
-print(top,last)
+def selected_group(data,filters):
+	selected=[]
+	recognized=[]
+	unrecognized=[]
+	for coin in data:
+		for name in filters:
+			if coin['symbol']==name:
+				selected.append(coin)
+				recognized.append(name)
+	for name in filters:
+		if name not in recognized:
+			unrecognized.append(name)
+	return selected,unrecognized
 
-def selected_group(data,filter):
-	#字串判斷
+data=rank_200()
+#top,last=group_rising_and_falling(data, 3, 100)
+#print(top,last)
+
+#condition是dict，格式->{'幣種1名稱':['上界數字','下界數字'], '第二幣種':['上界數字','下界數字']...以此類推}
+#return符合條件的到價資訊，如果沒有的話return false
+def price_zone(data,condition):
+	raised=[]
+	for data_coin in data:
+		if float(data_coin['price_twd']) > float(condition[data_coin['symbol']][0]) or float(data_coin['price_twd']) < float(condition[data_coin['symbol']][1]):
+			raised.append(data_coin)
+	if len(raised)>0:
+		return raised
+	else:
+		return false
+
+#讀入前一次push紀錄(避免重複push)
+#比對資訊，return:更新的push紀錄 與 需要傳送的資料
+def check_updates(last_updates,raised):
+	i=0
+	while i<len(last_updates)-1:
+		for r in raised:
+			if last_updates[i]['symbol']==r['symbol']:
+				last_updates.append(r)
+				if r['last_updated']>last_updates[i]['last_updated']:
+					last_updates.pop(i)
+					print('replace')
+				else:
+					raised.pop(r)
+					print('send')
+		i=i+1
+	return last_updates,raised
+
+
+
+
+
+
+
+
+
 
 #https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=TWD
 #BTC、ETH、LTC、
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,13 +1,60 @@
-import re
 import json
+import requests
+import schedule
+import time
+import datetime
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
+import crypt_api
+
+with open('data/setting.txt', 'r') as json_file:  
+    setting = json.load(json_file)
+
+mode=setting[0]['MODE']
+params=setting[1]['params']
+print(params)
+#with open('data/secret.txt', 'r') as json_file:
+#  secret = json.load(json_file)
+
+
+
 line_bot_api = LineBotApi('')
 def notification(content):
-    line_bot_api.push_message('to', TextSendMessage(text=content))
-    return True
+  line_bot_api.push_message('to', TextSendMessage(text=content))
+  return True
+
+def mode1_job():
+  new_fetch=crypt_api.rank_200()
+  filters=[]
+  for key in params:
+    filters.append(key)
+  #先篩出要的幣種資訊 如果有讀取失敗就print警告
+  selected,unrecognized=crypt_api.selected_group(new_fetch,filters)
+  if len(unrecognized)>0:
+    print("警告: 有未知幣種",unrecognized)
+  raised=crypt_api.price_zone(selected,params)
+  print('raised:',raised)
+  #讀取mode1的push紀錄(避免重複push)
+  with open('data/mode1_data.txt', 'r') as json_file:
+    data = json.load(json_file)
+    print(type(data))
+    updates,sends=crypt_api.check_updates(data, raised)
+    print('updates:',updates,'\n,send:',sends)
+  with open('data/mode1_data.txt', 'w') as json_file:
+    json_file.write(json.dumps(updates))
 
 
+if mode==1:
+  #mode1到價通知: params給定追蹤的貨幣及上下界區間，每分鐘更新，如果超出界線會push訊息
+  #請注意，雖然程式是每分鐘更新，但資料api來源「coinmarketcap」每種貨幣更新速率不一
+  print('start!!')
+  mode1_job()
+elif mode==2:
+  print("2")
+elif mode==3:
+  print("3")
+else:
+  print("wrong params")
 
