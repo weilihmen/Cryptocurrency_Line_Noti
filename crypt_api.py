4e5c86c0c7e14552bd92529d6bc54f8fd917b608
KC <weilihmen@gmail.com>
2018-03-01 23:48:22 +0800

    fix

--- crypt_api.py
+++ crypt_api.py
@@ -1,9 +1,7 @@
 import requests
 import json
 
-index01=200
-index02=['BTC','RLC','TEL','PPP','WRONGTEST']
-
+#抓Rank200內的資料
 def rank_200():
 	req = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=TWD')
 	req2 = requests.request('GET', 'https://api.coinmarketcap.com/v1/ticker/?convert=TWD&start=100&limit=100')
@@ -12,15 +10,17 @@ def rank_200():
 		new_data.append(coin)
 	return new_data
 
-def group_rising_and_falling(data,num,group_size):
+#取得一小時內漲與跌最大的幣種(以Rank為範圍)
+#num取排行的數量、group_size為rank的範圍
+def rising_and_falling(data,num,group_size):
 	group_data=data[:(group_size-1)]
 	newlist = sorted(group_data, key=lambda x: (float(x["percent_change_1h"]) >= 0, (float(x["percent_change_1h"])))) 
 	return newlist[-(num):],newlist[:num]
 
+#取得特定幣種資訊 filters放入幣種簡稱
 def selected_group(data,filters):
 	selected=[]
 	recognized=[]
-	unrecognized=[]
 	for coin in data:
 		for name in filters:
 			if coin['symbol']==name:
@@ -28,14 +28,11 @@ def selected_group(data,filters):
 				recognized.append(name)
 	for name in filters:
 		if name not in recognized:
-			unrecognized.append(name)
-	return selected,unrecognized
+			print("Alert: Unknown",name)
+	return selected
 
-data=rank_200()
-#top,last=group_rising_and_falling(data, 3, 100)
-#print(top,last)
-
-#condition是dict，格式->{'幣種1名稱':['上界數字','下界數字'], '第二幣種':['上界數字','下界數字']...以此類推}
+#到價通知
+#condition是dict，格式->{"幣種1名稱":["上界數字","下界數字""], "第二幣種":["上界數字","下界數字"]...以此類推}
 #return符合條件的到價資訊，如果沒有的話return false
 def price_zone(data,condition):
 	raised=[]
@@ -45,33 +42,28 @@ def price_zone(data,condition):
 	if len(raised)>0:
 		return raised
 	else:
-		return false
+		return False
 
-#讀入前一次push紀錄(避免重複push)
+#到價通知後 讀入前一次push紀錄(避免重複push)
 #比對資訊，return:更新的push紀錄 與 需要傳送的資料
 def check_updates(last_updates,raised):
-	i=0
-	while i<len(last_updates)-1:
+	deletes_a=[]
+	deletes_b=[]
+	for l in last_updates:
 		for r in raised:
-			last_updates.append(r)
-			if last_updates[i]['symbol']==r['symbol']:
-				if r['last_updated']>last_updates[i]['last_updated']:
-					last_updates.pop(i)
-					print('replace')
+			if l['symbol']==r['symbol']:
+				if int(l['last_updated'])==int(r['last_updated']):
+					deletes_b.append(r)
+					pass
 				else:
-					raised.pop(r) ##找到序列再刪除
-					print('send')
-		i=i+1
+					deletes_a.append(l)
+					pass
+			pass
+	for d in deletes_a:
+		last_updates.remove(d)
+	for d in deletes_b:
+		raised.remove(d)
+	for r in raised:
+		last_updates.append(r)
 	return last_updates,raised
 
-
-
-
-
-
-
-
-
-
-#https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=TWD
-#BTC、ETH、LTC、
--- main.py
+++ main.py
@@ -1,50 +1,63 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import json
-import requests
 import schedule
-import time
-import datetime
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
 import crypt_api
 
-with open('data/setting.txt', 'r') as json_file:  
-    setting = json.load(json_file)
+with open("data/setting.txt", "r") as json_file:  
+  setting = json.load(json_file)
 
-mode=setting[0]['MODE']
-params=setting[1]['params']
-print(params)
-#with open('data/secret.txt', 'r') as json_file:
-#  secret = json.load(json_file)
-
-
+mode=setting[0]["MODE"]
+params_1=setting[1]["params_1"]
+params_2=setting[2]["params_2"]
+params_3=setting[3]["params_3"]
 
 line_bot_api = LineBotApi('')
 def notification(content):
   line_bot_api.push_message('to', TextSendMessage(text=content))
   return True
 
+def parse(data):
+  parsed=[]
+  for d in data:
+    parsed.append({"Coin":d["symbol"],"Price":d["price_twd"],"Change_1H(%)":d["percent_change_1h"]})
+  return parsed
+
 def mode1_job():
   new_fetch=crypt_api.rank_200()
   filters=[]
-  for key in params:
+  for key in params_1:
     filters.append(key)
-  #先篩出要的幣種資訊 如果有讀取失敗就print警告
-  selected,unrecognized=crypt_api.selected_group(new_fetch,filters)
-  if len(unrecognized)>0:
-    print("警告: 有未知幣種",unrecognized)
-  raised=crypt_api.price_zone(selected,params)
-  print('raised:',raised)
-  #讀取mode1的push紀錄(避免重複push)
-  with open('data/mode1_data.txt', 'r') as json_file:
-    data = json.load(json_file)
-    print(type(data))
-    updates,sends=crypt_api.check_updates(data, raised)
-    print('updates:',updates,'\n,send:',sends)
-  with open('data/mode1_data.txt', 'w') as json_file:
-    json_file.write(json.dumps(updates))
+  #先篩出要的幣種資訊
+  selected=crypt_api.selected_group(new_fetch,filters)
+  raised=crypt_api.price_zone(selected,params_1)
+  #如果有到價通知，再做下一步
+  if raised!=False:
+    #讀取mode1的push紀錄(避免重複push)
+    with open('data/mode1_data.txt', 'r') as json_file:
+      data = json.load(json_file)
+      updates,sends=crypt_api.check_updates(data, raised)
+      sends=parse(sends)
+      print("Push:",sends)#noti
+    with open('data/mode1_data.txt', 'w') as json_file:
+      json_file.write(json.dumps(updates))
 
+def mode2_job():
+  new_fetch=crypt_api.rank_200()
+  filters=params_2
+  data=crypt_api.selected_group(new_fetch,filters)
+  data=parse(data)
+  print("Push",data)
+
+def mode3_job():
+  new_fetch=crypt_api.rank_200()
+  filters=params_3
+  tops,lasts=crypt_api.rising_and_falling(new_fetch,filters[0],filters[1])
 
 if mode==1:
   #mode1到價通知: params給定追蹤的貨幣及上下界區間，每分鐘更新，如果超出界線會push訊息
